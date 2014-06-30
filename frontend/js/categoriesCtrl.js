44b8a8d596c8c56c9c0a06b1057cb874d17ab932
Mahmoud Tantawy <mtantawy@users.noreply.github.com>
2014-06-30 23:46:12 +0200

    started working on categories, added add/edit/delete pages, collected all menus controllers in 1 js file

--- frontend/js/categoriesCtrl.js
+++ frontend/js/categoriesCtrl.js
@@ -15,3 +15,94 @@ elmenus.controller('categoriesCtrl', ['$scope', '$http', function($scope, $http)
             $scope.config = config;
             });
 }]);
+
+
+elmenus.controller('AddCategoryCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.addCategory = function() {
+        $http.post('http://localhost:4000/categories', [{"id":"", "name":$scope.category.name}]).
+        success(function(data, status, headers, config) {
+            $scope.AddResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.AddResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+    
+}]);
+
+elmenus.controller('DeleteCategoryCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.category = {};
+    $scope.category.id = $routeParams.id;
+    $http.get('http://localhost:4000/categories/'+$scope.category.id).
+        success(function(data, status, headers, config) {
+            $scope.categories = data[0];
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.categories = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+    $scope.deleteCategory = function() {
+        $http.delete('http://localhost:4000/categories/'+$scope.category.id).
+        success(function(data, status, headers, config) {
+            $scope.DeleteResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.DeleteResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+}]);
+
+
+elmenus.controller('EditCategoryCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.category = {};
+    $scope.category.id = $routeParams.id;
+    $http.get('http://localhost:4000/categories/'+$scope.category.id).
+        success(function(data, status, headers, config) {
+            $scope.categories = data[0];
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.categories = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+    $scope.editCategory = function() {
+        $http.put('http://localhost:4000/categories/'+$scope.category.id, [{"id":"", "name":$scope.category.name}]).
+        success(function(data, status, headers, config) {
+            $scope.EditResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.EditResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+}]);
--- frontend/js/menusCtrl.js
+++ frontend/js/menusCtrl.js
@@ -15,3 +15,93 @@ elmenus.controller('menusCtrl', ['$scope', '$http', function($scope, $http) {
             $scope.config = config;
             });
 }]);
+
+elmenus.controller('AddMenuCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.addMenu = function() {
+        $http.post('http://localhost:4000/menus', [{"id":"", "name":$scope.menu.name}]).
+        success(function(data, status, headers, config) {
+            $scope.AddResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.AddResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+    
+}]);
+
+elmenus.controller('DeleteMenuCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.menu = {};
+    $scope.menu.id = $routeParams.id;
+    $http.get('http://localhost:4000/menus/'+$scope.menu.id).
+        success(function(data, status, headers, config) {
+            $scope.menus = data[0];
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.menus = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+    $scope.deleteMenu = function() {
+        $http.delete('http://localhost:4000/menus/'+$scope.menu.id).
+        success(function(data, status, headers, config) {
+            $scope.DeleteResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.DeleteResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+}]);
+
+
+elmenus.controller('EditMenuCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.menu = {};
+    $scope.menu.id = $routeParams.id;
+    $http.get('http://localhost:4000/menus/'+$scope.menu.id).
+        success(function(data, status, headers, config) {
+            $scope.menus = data[0];
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.menus = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+    $scope.editMenu = function() {
+        $http.put('http://localhost:4000/menus/'+$scope.menu.id, [{"id":"", "name":$scope.menu.name}]).
+        success(function(data, status, headers, config) {
+            $scope.EditResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.EditResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+}]);
